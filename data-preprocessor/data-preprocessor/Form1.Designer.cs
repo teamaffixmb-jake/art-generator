4951c1292de7858bce36c7fe1ae2a5e99ede6ff1
teamaffixmb-jake <66917299+teamaffixmb-jake@users.noreply.github.com>
2022-09-10 13:07:44 -0700

    Added data preprocessor project in C#

---
+++ data-preprocessor/data-preprocessor/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace data_preprocessor
+{
+    partial class frmImagePreprocessor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectOutputDirectory = new System.Windows.Forms.Button();
+            this.btnSelectInputDirectory = new System.Windows.Forms.Button();
+            this.btnProcessAndDump = new System.Windows.Forms.Button();
+            this.lblInputDirectory = new System.Windows.Forms.Label();
+            this.lblOutputDirectory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectOutputDirectory
+            // 
+            this.btnSelectOutputDirectory.Location = new System.Drawing.Point(16, 74);
+            this.btnSelectOutputDirectory.Name = "btnSelectOutputDirectory";
+            this.btnSelectOutputDirectory.Size = new System.Drawing.Size(187, 22);
+            this.btnSelectOutputDirectory.TabIndex = 0;
+            this.btnSelectOutputDirectory.Text = "Select Dataset Output Directory";
+            this.btnSelectOutputDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectOutputDirectory.Click += new System.EventHandler(this.btnSelectOutputDirectory_Click);
+            // 
+            // btnSelectInputDirectory
+            // 
+            this.btnSelectInputDirectory.Location = new System.Drawing.Point(11, 23);
+            this.btnSelectInputDirectory.Name = "btnSelectInputDirectory";
+            this.btnSelectInputDirectory.Size = new System.Drawing.Size(187, 22);
+            this.btnSelectInputDirectory.TabIndex = 1;
+            this.btnSelectInputDirectory.Text = "Select Dataset Input Directory";
+            this.btnSelectInputDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectInputDirectory.Click += new System.EventHandler(this.btnSelectInputDirectory_Click);
+            // 
+            // btnProcessAndDump
+            // 
+            this.btnProcessAndDump.Location = new System.Drawing.Point(108, 129);
+            this.btnProcessAndDump.Name = "btnProcessAndDump";
+            this.btnProcessAndDump.Size = new System.Drawing.Size(187, 22);
+            this.btnProcessAndDump.TabIndex = 2;
+            this.btnProcessAndDump.Text = "Process and Dump Data";
+            this.btnProcessAndDump.UseVisualStyleBackColor = true;
+            this.btnProcessAndDump.Click += new System.EventHandler(this.btnProcessAndDump_Click);
+            // 
+            // lblInputDirectory
+            // 
+            this.lblInputDirectory.AutoSize = true;
+            this.lblInputDirectory.Location = new System.Drawing.Point(12, 48);
+            this.lblInputDirectory.Name = "lblInputDirectory";
+            this.lblInputDirectory.Size = new System.Drawing.Size(79, 13);
+            this.lblInputDirectory.TabIndex = 3;
+            this.lblInputDirectory.Text = "Input Directory:";
+            // 
+            // lblOutputDirectory
+            // 
+            this.lblOutputDirectory.AutoSize = true;
+            this.lblOutputDirectory.Location = new System.Drawing.Point(13, 99);
+            this.lblOutputDirectory.Name = "lblOutputDirectory";
+            this.lblOutputDirectory.Size = new System.Drawing.Size(87, 13);
+            this.lblOutputDirectory.TabIndex = 4;
+            this.lblOutputDirectory.Text = "Output Directory:";
+            // 
+            // frmImagePreprocessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 174);
+            this.Controls.Add(this.lblOutputDirectory);
+            this.Controls.Add(this.lblInputDirectory);
+            this.Controls.Add(this.btnProcessAndDump);
+            this.Controls.Add(this.btnSelectInputDirectory);
+            this.Controls.Add(this.btnSelectOutputDirectory);
+            this.Name = "frmImagePreprocessor";
+            this.Text = "Image Preprocessor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectOutputDirectory;
+        private System.Windows.Forms.Button btnSelectInputDirectory;
+        private System.Windows.Forms.Button btnProcessAndDump;
+        private System.Windows.Forms.Label lblInputDirectory;
+        private System.Windows.Forms.Label lblOutputDirectory;
+    }
+}
+
---
+++ data-preprocessor/data-preprocessor/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace data_preprocessor
+{
+    public partial class frmImagePreprocessor : Form
+    {
+        public frmImagePreprocessor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectInputDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog l_ofd = new FolderBrowserDialog();
+            if (l_ofd.ShowDialog() == DialogResult.OK)
+            {
+                lblInputDirectory.Text = l_ofd.SelectedPath;
+            }
+        }
+
+        private void btnSelectOutputDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog l_ofd = new FolderBrowserDialog();
+            if (l_ofd.ShowDialog() == DialogResult.OK)
+            {
+                lblOutputDirectory.Text = l_ofd.SelectedPath;
+            }
+        }
+
+        List<List<List<int>>> ConvertImage(Image a_image)
+        {
+            List<List<List<int>>> l_result = new List<List<List<int>>>();
+
+            int l_acceptable_processed_height = 100;
+            int l_acceptable_processed_width = 100;
+
+            // Add R, G, B channels
+            l_result.Add(new List<List<int>>());
+            l_result.Add(new List<List<int>>());
+            l_result.Add(new List<List<int>>());
+
+            // Add labels for the individual channel matrices
+            List<List<int>> l_red_matrix = l_result[0];
+            List<List<int>> l_green_matrix = l_result[1];
+            List<List<int>> l_blue_matrix = l_result[2];
+
+            Bitmap l_bitmap = new Bitmap(a_image);
+
+            for (int i = 0; i < l_acceptable_processed_height; i++)
+            {
+                l_red_matrix.Add(new List<int>());
+                l_green_matrix.Add(new List<int>());
+                l_blue_matrix.Add(new List<int>());
+                for (int j = 0; j < l_acceptable_processed_width; j++)
+                {
+                    Color l_color = l_bitmap.GetPixel(j, i);
+                    l_red_matrix[i].Add((int)l_color.R);
+                    l_green_matrix[i].Add((int)l_color.G);
+                    l_blue_matrix[i].Add((int)l_color.B);
+                }
+            }
+
+            return l_result;
+
+        }
+
+        void WriteProcessedImageToFile(string a_file_path, List<List<List<int>>> a_processed_image)
+        {
+            BinaryWriter l_writer = new BinaryWriter(new FileStream(a_file_path, FileMode.Create));
+            l_writer.Write(a_processed_image[0].Count);
+            l_writer.Write(a_processed_image[0][0].Count);
+            for (int i = 0; i < a_processed_image.Count; i++)
+            {
+                for (int j = 0; j < a_processed_image[0].Count; j++)
+                {
+                    for (int k = 0; k < a_processed_image[0][0].Count; k++)
+                    {
+                        l_writer.Write(a_processed_image[i][j][k]);
+                    }
+                }
+            }
+            l_writer.Close();
+        }
+
+        private void btnProcessAndDump_Click(object sender, EventArgs e)
+        {
+            string[] l_files_in_input = Directory.GetFiles(lblInputDirectory.Text);
+            foreach (string l_input_path in l_files_in_input)
+            {
+                Image l_image = Image.FromFile(l_input_path);
+                var l_converted = ConvertImage(l_image);
+                WriteProcessedImageToFile(lblOutputDirectory.Text + "/" + Path.GetFileName(l_input_path) + "-processed.bin", l_converted);
+            }
+        }
+    }
+}
